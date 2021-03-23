8809c3f6022ba3fde9daff198d1e04e89723b685
doodream <rhomo17@naver.com>
2021-03-23 13:43:52 +0900

    2021-03-23 : Verify : 코드정리

--- Doodream/스킬트리.js
+++ Doodream/스킬트리.js
@@ -3,46 +3,30 @@ function solution(skill, skill_trees) {
     var skillArr = skill.split('');
 
     skill_trees.map(x => {
-        var skillIndex = [];
-        var next = false;
         var isIncludesAny = false;
-
         for (let i = 0; i < skillArr.length; i++) {
             var isIncludes = x.indexOf(skillArr[i]);
             if (isIncludes !== -1) {
                 isIncludesAny = true;
                 if (i === 0) {
-                    skillIndex.push(isIncludes);
                     continue;
-                }
-                if (i > 0) {
-                    if (x.indexOf(skillArr[i - 1]) !== -1) {
-                        skillIndex.push(isIncludes);
-                    } else {
-                        next = true;
-                        break;
+                } else {
+                    var isIncludesPreskill = x.indexOf(skillArr[i - 1]);
+                    if (isIncludesPreskill === -1) {
+                        return;
+                    }
+                    if (isIncludesPreskill > isIncludes) {
+                        return;
                     }
                 }
             }
         }
-        if (isIncludesAny === false && skillIndex.length === 0) {
-            //console.log(x);
+        if (isIncludesAny === false) {
             answer++;
             return
         }
-
-        if (!next) {
-            var notSortSkillIndex = skillIndex.slice();
-            skillIndex.sort();
-            if (notSortSkillIndex.join('') === skillIndex.join('')) {
-                //console.log(x);
-                answer++;
-                return
-            }
-        }
-
+        answer++;
     })
-
     return answer;
 }
 
