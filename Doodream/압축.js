b63852bf5161e9e131f59cdc9af588e8a97bd476
Doodream <rhomo17@naver.com>
2021-04-06 17:52:10 +0900

    2021-04-06

--- Doodream/압축.js
+++ Doodream/압축.js
@@ -1,54 +1,36 @@
+const key = {
+    'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10,
+    'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20,
+    'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26
+}
+let idx = 27;
 function solution(msg) {
-    var msgStr = JSON.parse(JSON.stringify(msg));
-    function longestWord(count) {
-        var maxLength = dict[dict.length - 1].length
-        while (maxLength > 0) {
-            var word = msgStr.substring(count, count + maxLength);
-            if (dict.includes(word)) {
-                return word;
-            }
-            maxLength--;
-        }
+    let answer = [];
+    for (let i = 0; i < msg.length; i) {
+        let w = msg[i];
+        let c = msg[i + 1];
+        let newW = add(msg, i, w, c, 0);
+        // 반환된 문자 w의 색인 번호를 추가하고
+        // w의 길이만큼 건너뛰도록 i에 크기를 더한다
+        answer.push(key[newW]);
+        i += newW.length;
     }
-
-    msg = msg.split('');
-
-    var answer = [];
-    const dict = [];
-    for (let i = 0; i < 26; i++) {
-        dict[i] = String.fromCharCode([i + 65]);
-    }
-    var count = 0;
-    var currentEnter = longestWord(count);
-    var nextEnter = msg[count + 1];
-
-    while (true) {
-        if (count > msg.length) break;
-        // 현재 입력이 사전에 있나?
-        if (dict.includes(currentEnter)) {
-            // 다음번 입력이 포함된 길이가 사전에 있나?
-            if (!dict.includes(currentEnter + nextEnter)) {
-                // 출력
-                answer.push(dict.indexOf(currentEnter) + 1);
-                // 사전추가
-                dict.push(currentEnter + nextEnter);
-                count += currentEnter.length;
-
-                currentEnter = longestWord(count);
-                if (currentEnter === undefined) {
-                    return answer;
-                }
-                if (count + currentEnter.length > msg.length) {
-                    answer.push(dict.indexOf(currentEnter) + 1);
-                    return answer;
-                }
-                nextEnter = msg[count + currentEnter.length];
-                continue;
-            }
-        }
+    return answer;
+}
+function add(msg, i, w, c, cnt) {
+    // w+c가 사전에 있으면 그다음 단어를 붙어 다시 확인하고
+    // w+c가 사전에 없으면 사전에 추가하고 문자 w를 반환한다
+    if (key[w + c] === undefined) {
+        key[w + c] = idx++;
+        console.log(w, c, key[w], key[w + c])
+        return w;
+    } else {
+        cnt++;
+        let newW = w + c;
+        let newC = msg[i + 1 + cnt];
+        return add(msg, i, newW, newC, cnt);
     }
 }
-
 
 
 var msg = 'KAKAO';
