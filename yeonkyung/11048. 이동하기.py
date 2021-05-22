edcec820b5854f54f8066b129209b4fd598f58e0
yeonkyung <younkyong94@gmail.com>
2021-05-22 18:51:40 +0900

    2021.05.21

--- yeonkyung/11048. 이동하기.py
+++ yeonkyung/11048. 이동하기.py
@@ -1,12 +1,25 @@
 if __name__ == "__main__":
+    N, M = map(int, input().split())
+    candy = []
+    for _ in range(N):
+        candy.append(list(map(int, input().split())))
+    # print(candy)
 
-    n, m = map(int, input().split())
-    s = []
-    for i in range(n):
-        s.append(list(map(int, input().split())))
-    dp = [[0] * (m + 1) for i in range(n + 1)]
+    dp = [[0 for _ in range(M)] for _ in range(N)]
+    dp[0][0] = candy[0][0]
 
-    for i in range(1, n + 1):
-        for j in range(1, m + 1):
-            dp[i][j] = s[i - 1][j - 1] + max(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
-    print(dp[n][m])
+    for i in range(1, len(candy)):
+        dp[i][0] = dp[i-1][0] + candy[i][0]
+
+    for j in range(1, len(candy[0])):
+        dp[0][j] = dp[0][j-1] + candy[0][j]
+
+    for i in range(1, len(candy)):
+        for j in range(1, len(candy[i])):
+            for x, y in [(1, 1), (1, 0), (0, 1)]:
+                dp[i][j] = max(dp[i][j], dp[i-x][j-y] + candy[i][j])
+
+    print(dp[N-1][M-1])
+
+
+
--- yeonkyung/가장 긴 팰린드롬.py
+++ yeonkyung/가장 긴 팰린드롬.py
@@ -1,39 +1,98 @@
 '''
-def solution(s):
-
-    dp = [[0 for _ in range(len(s))] for _ in range(len(s))]
-
-    for i in range(len(s)):
-
-        start, end = i, i
-        dp[start][end] = 1
-
-        while True:
-            if start-1 >= 0 and end+1 <= len(s)-1 and s[start-1] == s[end+1]:
-                dp[start-1][end+1] = max(dp[start][end] + 2, dp[start-1][end+1])
-                start -= 1
-                end += 1
-            elif start-1 >= 0 and end <= len(s)-1 and s[start-1] == s[end]:
-                dp[start-1][end] = max(dp[start][end] + 1, dp[start-1][end])
-                start -= 1
-            elif start >= 0 and end + 1 <= len(s)-1 and s[start] == s[end+1]:
-                dp[start][end+1] = max(dp[start][end] + 1, dp[start][end+1])
-                end += 1
-            else:
-                break
-
-    answer = 0
-    for i in range(len(dp)):
-        for j in range(len(dp[i])):
-            answer = max(answer, dp[i][j])
-    return answer
-'''
+#1 - 브루트포스 1
 def solution(s):
     answer = 1
     for i in range(len(s)):
         for j in range(i, len(s)+1):
             if s[i:j] == s[i:j][::-1]:
                 answer = max(answer, len(s[i:j]))
+    return answer
+
+#2 - 브루트포스 2
+def solution(s):
+    for i in range(len(s), 0, -1):
+        for j in range(len(s)-i+1):
+            if s[j:j+i] == s[j:j+i][::-1]:
+                return i
+
+
+#3 - 재귀
+def solution(s):
+    if s == s[::-1]:
+        return len(s)
+    return max(solution(s[1:], solution(s[:-1])))
+'''
+
+#4 - DP(TD)
+def solution(s):
+    dp = [[0 for _ in range(len(s))] for _ in range(len(s))]
+
+    def recursive(start, end):
+
+        if dp[start][end] != 0:
+            return dp[start][end]
+
+        # 길이가 1
+        if start == end:
+            dp[start][end] = 1
+            return dp[start][end]
+
+        # 길이가 2
+        if start+1 == end:
+            if s[start] == s[end]:
+                dp[start][end] = 1
+                return dp[start][end]
+            else:
+                dp[start][end] = 0
+                return dp[start][end]
+
+        # 길이가 3 이상
+        if s[start] != s[end]:
+            dp[start][end] = 0
+            return dp[start][end]
+
+        dp[start][end] = recursive(start+1, end-1)
+        return dp[start][end]
+
+    answer = 0
+    for i in range(len(s)):
+        for j in range(len(s)-1, i-1, -1):
+
+            if recursive(i, j) == 1:
+                answer = max(answer, j - i + 1)
+
+    return answer
+
+#5 - DP(BU)
+def solution(s):
+    dp = [[0 for _ in range(len(s))] for _ in range(len(s))]
+
+    # 길이가 1
+    for i in range(len(s)):
+        dp[i][i] = 1
+
+    # 길이가 2
+    for i in range(len(s)-1):
+        if s[i] == s[i+1]:
+            dp[i][i+1] = 1
+        else:
+            dp[i][i+1] = 0
+
+    # 길이가 3 이상
+    for k in range(3, len(s)+1):
+        for i in range(len(s)-k+1):
+            j = i+k-1
+
+            if s[i] == s[j] and dp[i+1][j-1] == 1:
+                dp[i][j] = 1
+            else:
+                dp[i][j] = 0
+
+    answer = 1
+    for i in range(len(s)):
+        for j in range(len(s)-1, i-1, -1):
+            if dp[i][j] == 1:
+                answer = max(answer, j - i + 1)
     return answer
 
 if __name__ == "__main__":
