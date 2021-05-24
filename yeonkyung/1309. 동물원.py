1c1cc7e971270a5698d221e8410d6f42f7f088c5
yeonkyung <younkyong94@gmail.com>
2021-05-25 07:00:31 +0900

    2021.05.24

---
+++ yeonkyung/1309. 동물원.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+    N = int(input())
+    dp = [0 for _ in range(N+1)]
+
+    dp[0] = 1
+    dp[1] = 3
+
+    for i in range(2, N+1):
+        dp[i] = dp[i-1] * 2 + dp[i-2]
+
+    print(dp[N])
+
+
