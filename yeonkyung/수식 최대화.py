0af46d4c2aeef3dd2e11a5b3e7e7596bd787589d
yeonkyung <younkyong94@gmail.com>
2021-05-29 19:48:56 +0900

    2021.05.28

--- yeonkyung/수식 최대화.py
+++ yeonkyung/수식 최대화.py
@@ -1,32 +1,50 @@
-import re
 from itertools import permutations
+import sys
+
+def split(expression):
+
+    oper = []
+    nums = []
+    tmp = 0
+    for i in range(len(expression)):
+        if expression[i] in ['-', '*', '+']:
+            oper.append(expression[i])
+            nums.append(int(expression[tmp:i]))
+            tmp = i+1
+    nums.append(int(expression[tmp:]))
+
+    result = [nums[0]]
+    for i in range(len(oper)):
+        result.append(oper[i])
+        result.append(nums[i+1])
+    return oper, result
 
 def solution(expression):
-    oper = list()
-    if '*' in expression:
-        oper.append('*')
-    if '+' in expression:
-        oper.append('+')
-    if '-' in expression:
-        oper.append('-')
 
-    oper = list(permutations(oper))
+    oper, exp = split(expression)
+    orders = list(set(oper))
+    # print(oper, exp, orders)
 
-    expression = re.split('([^0-9])', expression)
-    max_sum = 0
-    for ops in oper:
-        expression_copy = expression[:]
-        for op in ops:
-            while op in expression_copy:
-                op_idx = expression_copy.index(op)
-                cal = str(eval(expression_copy[op_idx-1] + expression_copy[op_idx] +expression_copy[op_idx+1]))
-                expression_copy[op_idx-1] = cal
-                expression_copy = expression_copy[:op_idx] + expression_copy[op_idx+2:]
-
-        max_sum = max(max_sum, abs((int(expression_copy[-1]))))
-
-    return max_sum
+    answer = -sys.maxsize
+    for order in list(permutations(orders)):
+        ex = exp
+        for op in order:
+            stack = []
+            i = 0
+            while i < len(ex):
+                v = ex[i]
+                if v == op:
+                    n1 = str(stack.pop())
+                    n2 = str(ex[i+1])
+                    stack.append(eval(n1 + v + n2))
+                    i += 2
+                else:
+                    stack.append(v)
+                    i += 1
+            ex = stack
+        answer = max(answer, abs(ex[0]))
+    return answer
 
 if __name__ == "__main__":
-    expression = "50*6-3*2"
+    expression = "100-200*300-500+20"
     print(solution(expression))
