42c3736a2ceaf4a806ad2e924e8034383a58139b
yeonkyung <younkyong94@gmail.com>
2021-06-01 01:21:51 +0900

    2021.05.26

--- yeonkyung/게임 맵 최단거리.py
+++ yeonkyung/게임 맵 최단거리.py
@@ -1,31 +1,36 @@
 from collections import deque
 
-d = [[1,0], [-1, 0], [0,1], [0,-1]]
 
 def solution(maps):
-    r = len(maps)
-    c = len(maps[0])
-    table = [[-1 for _ in range(c)] for _ in range(r)]
-    q = deque()
-    q.append([0,0])
-    table[0][0] = 1
 
-    while q:
-        y, x = q.popleft()
+    dx = [-1, 0, 1, 0]
+    dy = [0, -1, 0, 1]
 
+    n, m = len(maps), len(maps[0])
+    queue = deque()
+    ch = [[-1 for _ in range(m)] for _ in range(n)]
+    queue.append((0, 0))
+    ch[0][0] = 1
+    maps[0][0] = 2
+    while queue:
+        x, y = queue.popleft()
+        if x == n - 1 and y == m - 1:
+            break
         for i in range(4):
-            ny = y + d[i][0]
-            nx = x + d[i][1]
+            xx = x + dx[i]
+            yy = y + dy[i]
+            if 0 <= xx < n and 0 <= yy < m and maps[xx][yy] == 1:
+                queue.append((xx, yy))
+                ch[xx][yy] = ch[x][y] + 1
+                maps[xx][yy] = 2
 
-            if -1<ny<r and -1<nx<c:
-                if maps[ny][nx] == 1:
-                    if table[ny][nx] == -1:
-                        table[ny][nx] = table[y][x] + 1
-                        q.append([ny, nx])
-
-    answer = table[-1][-1]
+    if maps[n - 1][m - 1] == 2:
+        answer = ch[n - 1][m - 1]
+    else:
+        answer = -1
     return answer
 
+
 if __name__ == "__main__":
-    numbers = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]
-    print(solution(numbers))
+    maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]
+    print(solution(maps))
