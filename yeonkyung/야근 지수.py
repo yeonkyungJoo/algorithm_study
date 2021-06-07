4c7a15c852705c47ba1d774e4392a97600e3f3fb
yeonkyung <younkyong94@gmail.com>
2021-06-08 06:33:13 +0900

    2021.06.07

---
+++ yeonkyung/야근 지수.py
@@ -0,0 +1,25 @@
+import heapq
+
+def solution(n, works):
+    for i in range(len(works)):
+        works[i] *= -1
+    heapq.heapify(works)
+
+    for i in range(0, n):
+        m = heapq.heappop(works)
+        if m >= 0:
+            heapq.heappush(works, m)
+            break
+        m += 1
+        heapq.heappush(works, m)
+
+    answer = 0
+    while len(works) > 0:
+        m = heapq.heappop(works)
+        answer += pow(m * -1, 2)
+    return answer
+
+if __name__ == "__main__":
+    works = [4, 3, 3]
+    n = 4
+    print(solution(n, works))
