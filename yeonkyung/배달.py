82c453a5534f8b902f52aeee60ad7bafc00a8a2e
yeonkyung <younkyong94@gmail.com>
2021-06-17 23:52:00 +0900

    2021.04.16

--- yeonkyung/배달.py
+++ yeonkyung/배달.py
@@ -1,26 +1,65 @@
-from collections import defaultdict
-from collections import deque
-
+'''
+# 시간 초과
 def solution(N, road, K):
 
-    nodes = defaultdict(list)
-    for s, d, di in road:
-        nodes[s].append((d, di))
-        nodes[d].append((s, di))
-    dist = { i:float('inf') if i != 1 else 0 for i in range(1, N+1) }
+    dir = dict()
+    for a, b, c in road:
+        if a not in dir:
+            dir[a] = []
+        if b not in dir:
+            dir[b] = []
 
-    queue = deque([1])
-    while queue:
-        cur = queue.popleft()
-        for next, di in nodes[cur]:
-            if dist[next] > dist[cur] + di:
-                dist[next] = dist[cur] + di
-                queue.append(next)
+        dir[a].append((b, c))
+        dir[b].append((a, c))
 
-    return len([True for dist in dist.values() if dist <= K])
+    for k, v in dir.items():
+        print(k, v)
+
+    ch = [0 for _ in range(N+1)]
+    answer = set()
+    def dfs(e, total):
+
+        if total <= K:
+            answer.add(e)
+        if total > K:
+            return
+
+        for i, j in dir[e]:
+            if ch[i] == 0:
+                ch[i] = 1
+                dfs(i, total + j)
+                ch[i] = 0
+
+    ch[1] = 1
+    dfs(1, 0)
+    return len(answer)
+'''
+
+def solution(N, road, K):
+    answer = 0
+    dis = [[500001 for _ in range(N+1)] for _ in range(N+1)]
+
+    for i in range(1, len(dis)):
+        dis[i][i] = 0
+
+    for r in road:
+        v1, v2, t = r[0], r[1], r[2]
+        dis[v1][v2] = min(dis[v1][v2], t)
+        dis[v2][v1] = min(dis[v2][v1], t)
+
+    for k in range(1, N+1):
+        for i in range(1, N+1):
+            for j in range(1, N+1):
+                dis[i][j] = min(dis[i][j], dis[i][k] + dis[k][j])
+
+    for i in range(1, N+1):
+        if dis[1][i] <= K:
+            answer += 1
+
+    return answer
 
 if __name__ == "__main__":
-    N = 5
-    road = [[1,2,1],[2,3,3],[5,2,2],[1,4,2],[5,3,1],[5,4,2]]
-    K = 3
+    N = 6
+    road = [[1,2,1],[1,3,2],[2,3,2],[3,4,3],[3,5,2],[3,5,3],[5,6,1]]
+    K = 4
     print(solution(N, road, K))
